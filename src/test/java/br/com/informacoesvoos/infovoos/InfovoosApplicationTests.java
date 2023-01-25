377765be27220cc0038c8cef731fc23fc8d4b4b8
victorber21 <victorber21@gmail.com>
2023-01-25 11:57:54 -0300

    criação inicial

---
+++ src/test/java/br/com/informacoesvoos/infovoos/InfovoosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.informacoesvoos.infovoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfovoosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
